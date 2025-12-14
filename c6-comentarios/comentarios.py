b2c2144b23bc2301a15e195685344c3c905deaf2
Audiel-MT <audielmartinez19@gmail.com>
2025-12-14 15:05:34 -0600

    Realizando comentarios de una sola línea y multilínea

---
+++ c6-comentarios/comentarios.py
@@ -0,0 +1,32 @@
+# ¿Por qué usar comentarios en Python?
+"""
+Comentar sirve para mejorar la legibilidad,
+documentar decisiones y desactivar código sin borrarlo.
+Python omite todo lo que está después del numeral en la misma línea,
+por lo que no afecta la ejecución ni la salida en la terminal.
+También es útil cuando estás probando cambios
+o aparece un error y necesitas aislarlo.
+"""
+
+print("Hola mundo") # lo podemos utilizar para comentar que imprime el código.
+
+# Utilizando el 
+# numeral 
+# podemos
+# hacer 
+# comentarios
+# de una sola linea.
+
+"""
+Utilizando comillas triples
+podemos hacer comentarios
+multilínea.
+"""
+
+'''
+Y también utilizando comillas
+simples podemos hacer comentarios
+multilínea.
+'''
+
+# print("Hola mundo comentado") de esta manera podemos evitar código que se ejecute.
