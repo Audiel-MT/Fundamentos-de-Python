c25e13f653ceb1e3491acd059f2b838f7e0dd5a6
Audiel-MT <audielmartinez19@gmail.com>
2025-12-14 11:26:29 -0600

    Sintaxis e indentación en Python

---
+++ c5-sintaxis-indentación/sintaxis_indentación.py
@@ -0,0 +1,16 @@
+# ¿qué es la sintaxis e indentación en Python y cómo se usan?
+"""
+La sintaxis son las reglas que Python entiende como instrucciones.
+La indentación es parte de esa sintaxis: son los espacios o tabulaciones
+que agrupan líneas en un bloque de código.
+Sin indentación correcta, Python no entiende el programa
+"""
+
+# Ejemplo: sintaxis e indentación correcta.
+if 5 > 3:
+    print("sintaxis e indentación correcta")
+
+# Ejemplo: sintaxis e indentación incorrecta.
+if 10 > 4:
+print("sintaxis e indentación incorrecta")
+# Nos arrojara un error en la terminal de IndentationError.
